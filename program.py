6b462dbb3611dbf5899dca3b8823770371d25e3e
Iva Fischerová <fischerova.ivka@gmail.com>
2019-04-12 20:45:09 +0200

    doplneno jmeno

--- program.py
+++ program.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import math 
-=======
 import sys 
 import random
 cislo = random.randint(int(sys.argv[1]),int(sys.argv[2])) 
 print(cislo)
->>>>>>> 7b20ff2c1628658cd30000c26f8dcb493b3d7b51
+jmeno = 'iva' 
